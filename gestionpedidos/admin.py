bc19e875890f8429725e498a2f8a7677eb701fb9
ramon-diaz <ramndiaz@gmail.com>
2021-11-18 14:42:39 -0400

    second commit

--- gestionpedidos/admin.py
+++ gestionpedidos/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from gestionpedidos.models import Clientes
+from gestionpedidos.models import Articulos
+from gestionpedidos.models import pedidos
 
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=("nombre", "telefono", "email", "direccion")
+    search_fields=("nombre", "telefono")
+    list_filter=("nombre",)
+    #ponemos solo lo que queremos que se vea en el orden que queramos
+    #esto se visualiza dentro de edmin
+class ArticulosAdmin(admin.ModelAdmin):
+    list_display=("nombre", "seccion", "precio")
+    search_fields=("nombre", "precio")
+    list_filter=("nombre",)
+class PedidosAdmin(admin.ModelAdmin):
+    list_display=("numero", "fecha", "entregado")
+    search_fields=("numero", "fecha")
+    list_filter=("fecha",)
+    date_hierarchy="fecha"
 # Register your models here.
+
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(pedidos, PedidosAdmin)
+
+
--- gestionpedidos/models.py
+++ gestionpedidos/models.py
@@ -4,12 +4,16 @@ from django.db import models
 class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    email = models.EmailField()
-    telefono = models.CharField(max_length=7)
+    email = models.EmailField(blank=True, null=True)
+    telefono = models.CharField(max_length=7, verbose_name='contacto')
+    def __str__(self):
+        return 'nombre: %s , direccion: %s , email: %s telefono: %s' %(self.nombre, self.direccion, self.email, self.telefono)
 class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
+    def __str__(self):
+        return 'nombre: %s , seccion: %s , precio: %s' %(self.nombre, self.seccion, self.precio)
 class pedidos(models.Model):
     numero = models.IntegerField()
     fecha = models.DateField()
